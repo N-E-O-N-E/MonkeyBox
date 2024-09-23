b5a5f5fbacdb07471453fb50f6281a6cde65c6eb
Markus Wirtz <info@markuswirtz.net>
2024-09-23 13:45:21 +0200

    Initial Commit

---
+++ MonkeyBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MonkeyBox
+//
+//  Created by Markus Wirtz on 23.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MonkeyBox/MonkeyBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonkeyBoxApp.swift
+//  MonkeyBox
+//
+//  Created by Markus Wirtz on 23.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MonkeyBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
