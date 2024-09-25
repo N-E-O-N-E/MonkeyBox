0eb167f12008cbf17fe49e81bd1aec81b0176a25
Markus Wirtz <info@markuswirtz.net>
2024-09-26 00:21:53 +0200

    Datensicherung von NEONE

--- MonkeyBox/ContentView.swift
+++ MonkeyBox/ContentView.swift
@@ -16,54 +16,52 @@ struct ContentView: View {
     @State private var isActive: Bool = false
     
     var body: some View {
-        NavigationStack {
-            Group {
-                if isActive == false {
-                    SplashView()
-                }else {
-                    if loginState {
-                        TabView(selection: $selectedTab) {
-                            HomeView()
-                                .tabItem {
-                                    Label("Storages", systemImage: "house.fill")
-                                }
-                                .tag(1)
-                               
-                            ItemListView()
-                                .tabItem {
-                                    Label("Items", systemImage: "doc.fill")
-                                }
-                                .tag(2)
-                                
-                            SettingsView()
-                                .tabItem {
-                                    Label("Settings", systemImage: "gearshape.fill")
-                                }
-                                .tag(3)
-                                
-                        }.navigationBarBackButtonHidden()
-                        
+            NavigationStack {
+                Group {
+                    if !isActive {
+                        SplashView()
                     } else {
-                        LoginView()
-                            .modelContainer(for: [User.self])
+                        if loginState {
+                            TabView(selection: $selectedTab) {
+                                HomeView()
+                                    .tabItem {
+                                        Label("Storages", systemImage: "house.fill")
+                                    }
+                                    .tag(1)
+                                   
+                                ItemListView()
+                                    .tabItem {
+                                        Label("Items", systemImage: "doc.fill")
+                                    }
+                                    .tag(2)
+                                    
+                                SettingsView()
+                                    .tabItem {
+                                        Label("Settings", systemImage: "gearshape.fill")
+                                    }
+                                    .tag(3)
+                                    
+                            }.navigationBarBackButtonHidden()
+                            
+                        } else {
+                            LoginView()
+                        }
+                    }
+                }
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        isActive = true
                     }
                 }
             }
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                    isActive = true
-                }
-            }
-            
+            .modelContainer(for: [User.self, Item.self, Storage.self])
         }
-        .modelContainer(for: [User.self, Item.self, Storage.self])
     }
-}
 
 
 #Preview {
     let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
-    let container = try! ModelContainer(for: Storage.self, Item.self, configurations: configuration)
+    let container = try! ModelContainer(for: Storage.self, Item.self, User.self, configurations: configuration)
     return ContentView()
         .modelContainer(container)
 }
--- MonkeyBox/HomeView/HomeItemEditView.swift
+++ MonkeyBox/HomeView/HomeItemEditView.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import SwiftData
+
 struct HomeItemEditView: View {
     
     @Environment(\.modelContext) private var context
