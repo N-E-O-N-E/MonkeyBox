c117af102a76885d4552f54320d35f447d2a50e3
Kai Becker <KBecker92@web.de>
2024-09-26 11:17:01 +0200

    HomeItemView changed

--- MonkeyBox/HomeView/HomeItemView.swift
+++ MonkeyBox/HomeView/HomeItemView.swift
@@ -7,16 +7,45 @@
 
 import SwiftUI
 import SwiftData
-
 struct HomeItemView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @Environment(\.modelContext) private var context
+  var storage: Storage
+  @Query var items: [Item]
+  var body: some View {
+    ScrollView {
+      VStack {
+        Text("\(storage.name) Inventory")
+          .font(.title).bold()
+          .padding()
+        if items.isEmpty {
+          Text("No Room Items yet")
+        }else {
+          LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 10) {
+            ForEach(storage.items) { item in
+              VStack {
+                Image(item.image)
+                  .resizable()
+                  .scaledToFill()
+                  .frame(width: 34, height: 34)
+                Text(item.name)
+                  .font(.headline)
+                  .padding(.top, 5)
+              }
+              .frame(maxWidth: .infinity)
+              .background(Color.black.opacity(0.5))
+              .padding()
+            }
+          }
+        }
+      }
     }
+    .navigationTitle(storage.name)
+  }
 }
-
 #Preview {
-    let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
-    let container = try! ModelContainer(for: Storage.self, Item.self, User.self, configurations: configuration)
-    return HomeItemView()
-        .modelContainer(container)
+  let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
+  let container = try! ModelContainer(for: Storage.self, Item.self, configurations: configuration)
+  let storage = Storage(name: "Test Room", image: "MonkeyBoxLogo")
+  return HomeItemView(storage: storage)
+    .modelContainer(container)
 }
