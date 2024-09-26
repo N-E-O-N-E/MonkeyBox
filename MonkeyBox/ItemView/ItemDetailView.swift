e8b0ffa3bef57cfc216802bc506a2a44e7aa6ae9
Kai Becker <KBecker92@web.de>
2024-09-26 15:09:06 +0200

    ItemDetailView bearbeitet

--- MonkeyBox/ItemView/ItemDetailView.swift
+++ MonkeyBox/ItemView/ItemDetailView.swift
@@ -9,14 +9,75 @@ import SwiftUI
 import SwiftData
 
 struct ItemDetailView: View {
+    
+    @Environment(\.modelContext) private var context
+    @Query var storages: [Storage]
+    @State private var itemQuantity = 1
+
+    var item: Item
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+            Text(item.name)
+            .font(.title)
+            .fontWeight(.semibold)
+            
+            Image(item.image)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 80)
+        List{
+            Text("Description:")
+            
+            Text("Added:")
+                .font(.subheadline)
+            Text(item.date.formatted(date: .abbreviated, time: .omitted))
+                .font(.subheadline)
+            
+            Text("Quantity: \(item.quantity)")
+                .font(.subheadline)
+            Button(action: {
+                if itemQuantity > 1 {
+                    itemQuantity -= 1
+                }
+            }){
+                Text("-")
+                    .frame(width: 15, height: 5)
+                    .font(.title)
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    
+            }
+            
+            
+            
+            Button(action: {
+                if itemQuantity >= 1 {
+                    itemQuantity += 1
+                }
+            }){
+                Text("+")
+                    .frame(width: 15, height: 5)
+                    .font(.title)
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    
+            }
+         
+            
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("\(item.name) Details")
     }
 }
 
 #Preview {
     let configuration = ModelConfiguration(isStoredInMemoryOnly: true)
     let container = try! ModelContainer(for: Storage.self, Item.self, User.self, configurations: configuration)
-    return ItemDetailView()
+    let testItem = Item(name: "Test Item", descriptions: "A detailed description", image: "Datei_1", date: Date(), quantity: 1)
+    return ItemDetailView(item: testItem)
         .modelContainer(container)
 }
