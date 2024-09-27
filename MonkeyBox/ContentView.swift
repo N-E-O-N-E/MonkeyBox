d93a299a8d68f23591abc03a1f699f50f9175f8a
Milan Seel <milan.t.kadri@gmail.com>
2024-09-27 11:51:37 +0200

    changed homeview logic

--- MonkeyBox/ContentView.swift
+++ MonkeyBox/ContentView.swift
@@ -27,9 +27,8 @@ struct ContentView: View {
         } else {
             if loginState {
                 TabView(selection: $selectedTab) {
-                    NavigationStack {
+                
                         HomeView()
-                    }
                     .tabItem {
                         Label("Storages", systemImage: "house.fill")
                     }
--- MonkeyBox/HomeView/HomeView.swift
+++ MonkeyBox/HomeView/HomeView.swift
@@ -21,6 +21,8 @@ struct HomeView: View {
     
     @State private var selectedItems: [Storage] = []
     
+    @State private var selectedStorage: Storage?
+    
     let columns = [
         GridItem(.flexible()),
         GridItem(.flexible())
@@ -36,162 +38,168 @@ struct HomeView: View {
     }
     
     var body: some View {
-        ScrollView {
-            HStack {
-                Text("MonkeyBox").font(.system(size: 40)).bold()
-                    .padding(10)
+        NavigationStack {
+            ScrollView {
+                HStack {
+                    Text("MonkeyBox").font(.system(size: 40)).bold()
+                        .padding(10)
+                    
+                    Spacer()
+                }
                 
-                Spacer()
-            }
-            
-            LazyVGrid(columns: columns, spacing: 16) {
-                ForEach(storages) { item in
+                LazyVGrid(columns: columns, spacing: 16) {
+                    ForEach(storages) { item in
+                        Button(
+                            action: {
+                                if !animationOn {
+                                    selectedStorage = item
+                                } else {
+                                    withAnimation {
+                                        if let index = selectedItems.firstIndex(where: { $0.id == item.id }) {
+                                            selectedItems.remove(at: index)
+                                        } else {
+                                            selectedItems.append(item)
+                                        }
+                                    }
+                                }
+                            },
+                            label: {
+                                VStack(alignment:.center) {
+                                    ZStack {
+                                        Image(item.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 180, height: 140)
+                                            .cornerRadius(10)
+                                        
+                                        if selectedItems.contains(where: { $0.id == item.id }) {
+                                            Image(systemName: "checkmark.circle.fill")
+                                                .foregroundColor(.pink)
+                                                .font(.system(size: 40))
+                                                .background(Color.white.opacity(1.0))
+                                                .clipShape(Circle())
+                                                .padding(8)
+                                        }
+                                    }
+                                    HStack {
+                                        Text(item.name)
+                                            .font(.callout)
+                                            .foregroundStyle(.white)
+                                    }.padding(.horizontal, 10)
+                                }
+                                .frame(maxWidth: 180, maxHeight: 190)
+                                .background(LinearGradient(gradient: Gradient(colors: [Color(.white), Color(.systemGray3), Color(.black).opacity(0.7)]),startPoint: .top,endPoint: .bottom))
+                                .shadow(radius: 1)
+                                .cornerRadius(8)
+                                .padding(10)
+                                
+                                .rotationEffect(Angle(degrees: animationOn ? -5 : 0))
+                                .animation(animationOn ? Animation.easeInOut(duration: 0.2)
+                                    .repeatForever(autoreverses: true) : .default, value: animationOn)
+                                
+                                .onLongPressGesture (minimumDuration: 1.0) {
+                                    withAnimation {
+                                        animationOn = true
+                                    }
+                                }
+                            }
+                        )
+                        
+                        
+                        
+                    }
                     
                     VStack(alignment:.center) {
-                        ZStack {
-                            Image(item.image)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 180, height: 140)
-                                .cornerRadius(10)
+                        Button(action: {
+                            showAddSheet = true
+                        }) {
                             
-                            if selectedItems.contains(where: { $0.id == item.id }) {
-                                Image(systemName: "checkmark.circle.fill")
-                                    .foregroundColor(.pink)
-                                    .font(.system(size: 40))
-                                    .background(Color.white.opacity(1.0))
-                                    .clipShape(Circle())
-                                    .padding(8)
+                            ZStack(alignment:.center) {
+                                Image("Kongtainer")
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 180, height: 140)
+                                    .cornerRadius(10)
+                                    .blur(radius: 2)
+                                ZStack {
+                                    Image(systemName: "plus.app.fill")
+                                        .foregroundStyle(.white)
+                                        .font(.system(size: 125))
+                                        .opacity(0.9)
+                                }
                             }
                         }
-                        HStack {
-                            Text(item.name)
-                                .font(.callout).foregroundStyle(.white)
-                                
-                            Spacer()
-                            
-                            NavigationLink(destination: HomeItemView(storage: item.self)){
-                                
-                                Image(systemName: "arrowshape.right.circle.fill")
-                                    .foregroundStyle(.white)
-                            }.padding(5)
-                        }.padding(.horizontal, 10)
+                        .sheet(isPresented: $showAddSheet){
+                            HomeAddView()
+                        }
+                        Text("NEW")
+                            .font(.callout).foregroundStyle(.white)
+                            .padding(.bottom, 6)
                     }
                     .frame(maxWidth: 180, maxHeight: 190)
                     .background(LinearGradient(gradient: Gradient(colors: [Color(.white), Color(.systemGray3), Color(.black).opacity(0.7)]),startPoint: .top,endPoint: .bottom))
-                    .shadow(radius: 1)
                     .cornerRadius(8)
                     .padding(10)
+                    .shadow(radius: 1)
                     
-                    .rotationEffect(Angle(degrees: animationOn ? -5 : 0))
-                    .animation(animationOn ? Animation.easeInOut(duration: 0.2)
-                        .repeatForever(autoreverses: true) : .default, value: animationOn)
                     
-                    .onLongPressGesture (minimumDuration: 1.0) {
-                        withAnimation {
-                            animationOn = true
-                        }
-                    }
-                    
-                    .onTapGesture {
-                        if animationOn {
-                            withAnimation {
-                                if let index = selectedItems.firstIndex(where: { $0.id == item.id }) {
-                                    selectedItems.remove(at: index)
-                                } else {
-                                    selectedItems.append(item)
-                                }
-                            }
-                        }
-                    }
                 }
-                
-                VStack(alignment:.center) {
-                    Button(action: {
-                        showAddSheet = true
-                    }) {
-                        
-                        ZStack(alignment:.center) {
-                            Image("Kongtainer")
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 180, height: 140)
-                                .cornerRadius(10)
-                                .blur(radius: 2)
-                            ZStack {
-                                Image(systemName: "plus.app.fill")
-                                    .foregroundStyle(.white)
-                                    .font(.system(size: 125))
-                                    .opacity(0.9)
-                            }
-                        }
-                    }
-                    .sheet(isPresented: $showAddSheet){
-                        HomeAddView()
-                    }
-                    Text("NEW")
-                        .font(.callout).foregroundStyle(.white)
-                        .padding(.bottom, 6)
-                }
-                .frame(maxWidth: 180, maxHeight: 190)
-                .background(LinearGradient(gradient: Gradient(colors: [Color(.white), Color(.systemGray3), Color(.black).opacity(0.7)]),startPoint: .top,endPoint: .bottom))
-                .cornerRadius(8)
-                .padding(10)
-                .shadow(radius: 1)
-                
-                
-            }
-            .toolbar {
-                
-                ToolbarItem(placement: .topBarLeading) {
-                    if !selectedItems.isEmpty {
-                        
-                        HStack {
-                            Button(action: {
-                                withAnimation {
-                                    selectedItems.removeAll()
-                                    animationOn = false
-                                }
-                            }) {
-                                HStack{
-                                    Image(systemName: "xmark.square")
-                                        .foregroundStyle(.black)
-                                }
-                            }
+                .toolbar {
+                    
+                    ToolbarItem(placement: .topBarLeading) {
+                        if !selectedItems.isEmpty {
                             
-                            Button(action: {
-                                delSelectedItems()
-                                withAnimation {
-                                    animationOn = false
-                                }
-                            }) {
-                                HStack{
-                                    Image(systemName: "trash")
-                                        .foregroundStyle(.red)
-                                    Text("Selected: \(selectedItems.count)").font(.callout)}
-                            }
-                            
-                            Button(action: {
-                                withAnimation {
-                                    if selectedItems.count == 1{
-                                        showEditSheet = true
+                            HStack {
+                                Button(action: {
+                                    withAnimation {
+                                        selectedItems.removeAll()
                                         animationOn = false
                                     }
+                                }) {
+                                    HStack{
+                                        Image(systemName: "xmark.square")
+                                            .foregroundStyle(.black)
+                                    }
                                 }
-                            }) {
-                                HStack{
-                                    Image(systemName: "square.and.pencil")
-                                        .foregroundStyle(.blue)
-                                }.sheet(isPresented: $showEditSheet){
-                                    HomeItemEditView(selectedItem: selectedItems.first)
+                                
+                                Button(action: {
+                                    delSelectedItems()
+                                    withAnimation {
+                                        animationOn = false
+                                    }
+                                }) {
+                                    HStack{
+                                        Image(systemName: "trash")
+                                            .foregroundStyle(.red)
+                                        Text("Selected: \(selectedItems.count)").font(.callout)}
+                                }
+                                
+                                Button(action: {
+                                    withAnimation {
+                                        if selectedItems.count == 1{
+                                            showEditSheet = true
+                                            animationOn = false
+                                        }
+                                    }
+                                }) {
+                                    HStack{
+                                        Image(systemName: "square.and.pencil")
+                                            .foregroundStyle(.blue)
+                                    }.sheet(isPresented: $showEditSheet){
+                                        HomeItemEditView(selectedItem: selectedItems.first)
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
+            .background(LinearGradient(gradient: Gradient(colors: [Color.gray.opacity(0.2), Color.black.opacity(0.3)]),startPoint: .top, endPoint: .bottom))
+            .navigationDestination(item: $selectedStorage){
+                HomeItemView(storage: $0)
+            }
+            
         }
-        .background(LinearGradient(gradient: Gradient(colors: [Color.gray.opacity(0.2), Color.black.opacity(0.3)]),startPoint: .top, endPoint: .bottom))
     }
 }
 
